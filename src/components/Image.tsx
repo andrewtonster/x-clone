21c0d8ccd747407817a5603c3f0b367f775f8b90
andrewtonster <andrewtonster@gmail.com>
2025-04-16 13:38:59 -0700

    updating frontend ui

---
+++ src/components/Image.tsx
@@ -0,0 +1,30 @@
+"use client";
+import { Image as IKImage } from "@imagekit/next";
+import React from "react";
+
+type ImageType = {
+  path: string;
+  w: number;
+  h: number;
+  alt: string;
+  className?: string;
+  tr?: boolean;
+};
+
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
+
+const Image = ({ path, w, h, alt, className, tr }: ImageType) => {
+  return (
+    <IKImage
+      urlEndpoint={urlEndpoint}
+      src={path}
+      alt={alt}
+      className={className}
+      width={w}
+      height={h}
+      {...(tr ? { transformation: [{ width: w, height: h }] } : {})}
+    />
+  );
+};
+
+export default Image;
